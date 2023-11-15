1122b523fa6adf23e2ee2caff76b0e02052b6255
Liam Ramalingam <liamram28@gmail.com>
2023-11-15 12:13:08 +0200

    tester v2

--- todo-list-app/index.js
+++ todo-list-app/index.js
@@ -21,5 +21,5 @@ app.use('/test', appointRoutes); // Use the appoint routes for /api/appoints
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}  and it is working `);
 });
